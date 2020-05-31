15bec940bdb38be1d3c00e958f27e2ef152b95c5
Choi-UngGyu <cdr982116@gmail.com>
2020-05-31 22:34:24 +0900

    Add new feature

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ var csInterface = new CSInterface();
 
 var params = {
     interval: 1,
-    numberOfClipsToMultiply: 1
+    numberOfClipsToMultiply: 1,
+    toEndOfTheVideo: false
 }
 
 function start(event) {
@@ -14,7 +15,23 @@ function start(event) {
     csInterface.evalScript('$.scripts.everySecond(' + JSON.stringify(params) + ')');
 }
 
+function toEndOfTheVideoCheckboxChanged(event) {
+    var toEndOfTheVideoCheckbox = document.getElementById('to-end-of-the-video-checkbox');
+    var numberOfClipsToMultiplyInput = document.getElementById('number-of-clips-to-multiply-input');
+
+    if (toEndOfTheVideoCheckbox.checked) {
+        numberOfClipsToMultiplyInput.readOnly = true;
+        params.toEndOfTheVideo = true;
+    }
+    else {
+        numberOfClipsToMultiplyInput.readOnly = false;
+        params.toEndOfTheVideo = false;
+    }
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
     var startButton = document.getElementById('start-button');
     startButton.addEventListener('click', start);
+    var toEndOfTheVideoCheckbox = document.getElementById('to-end-of-the-video-checkbox');
+    toEndOfTheVideoCheckbox.addEventListener('change', toEndOfTheVideoCheckboxChanged);
 });
--- jsx/everysecond.jsx
+++ jsx/everysecond.jsx
@@ -4,21 +4,36 @@ Adobe Premiere Pro every second script
 
 $.scripts = {
     everySecond: function(args) {
+        app.enableQE();
+
         var activeSeq = app.project.activeSequence;
         var videoTrack = activeSeq.videoTracks[0];
         var clip = app.project.rootItem.children[0];
         var interval = parseFloat(args.interval);
+        const coefficient = 254016000000
 
         var currentTime = interval;
-        for (var i = 0; i < args.numberOfClipsToMultiply; i++) {
-            activeSeq.videoTracks[i + 1].insertClip(clip, currentTime);
-            currentTime += interval;
+
+        if (args.toEndOfTheVideo) {
+            var currentTime = interval;
+            var endTime = activeSeq.end / coefficient;
+            for (var i = 0; currentTime < endTime; i++) {
+                activeSeq.videoTracks[i + 1].insertClip(clip, currentTime);
+                currentTime += interval;
+            }
+        }
+        else {
+            for (var i = 0; i < args.numberOfClipsToMultiply; i++) {
+                activeSeq.videoTracks[i + 1].insertClip(clip, currentTime);
+                currentTime += interval;
+            }
         }
     }
 }
 
 // test
-// $.scripts.everySecond({
-//     interval: 0.1,
-//     numberOfClipsToMultiply: 10
-// });
+$.scripts.everySecond({
+    interval: 0.1,
+    numberOfClipsToMultiply: 10,
+    toEndOfTheVideo: true
+});
